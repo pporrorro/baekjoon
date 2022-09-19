d0e5e42803419d757a1587991e9d5f6ebae85c64
ppororo <110891908+pporrorro@users.noreply.github.com>
2022-09-19 16:51:27 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if 90 <= a and 100 >= a :
+  print('A')
+elif 80 <= a and 89 >= a :
+  print('B')
+elif 70 <= a and 79 >= a :
+  print('C')
+elif 60 <= a and 69 >= a :
+  print('D')
+else :
+  print('F')
